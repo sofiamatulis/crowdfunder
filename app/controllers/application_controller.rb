c0eaa2e2109564f227e4099de3276dcf28ba7c01
Adam Tse <mr.adam.tse@gmail.com>
2016-09-21 16:23:33 -0400

    added project validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  validates :name, :date, :goal, :user_id, :category_id, :description, presence: true
 end
