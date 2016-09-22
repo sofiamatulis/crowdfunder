e70ddada57415afa6503b81157bafb8273c4b02e
mitchell demers <mitchelldemers66@gmail.com>
2016-09-22 13:52:57 -0400

    added an href to the js file

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -13,7 +13,6 @@ $(function() {
     }).done(function(data){
 
       for(i = 0, l = data.length; i < l; i++){
-        console.log(data[i][''])
         $('<tr>').appendTo('.categories-table')
         $('<td>').attr('class', 'name').html(data[i]['name']).appendTo('.categories-table')
         $('<td>').attr('class', 'name').html(data[i]['date']).appendTo('.categories-table')
@@ -21,6 +20,7 @@ $(function() {
         $('<td>').attr('class', 'name').html(data[i]['user_id']).appendTo('.categories-table')
         $('<td>').attr('class', 'name').html(data[i]['category_id']).appendTo('.categories-table')
         $('<td>').attr('class', 'name').html(data[i]['description']).appendTo('.categories-table')
+        $('<a>').attr('class', 'show-btn').attr('href', '/projects/' + (data[i]['id'] +1) ).html('show').appendTo('.categories-table')
         $('<br>').appendTo('.categories-table')
       };
     }).fail(function(){
@@ -30,6 +30,7 @@ $(function() {
       $('.categories-table').html("")
       $('.all-projects-table-body').attr('style',"display")
     });
+
   });
 
 });
