66bfbe6c2b4e46780df955b0a4ff3b38297ef547
Adam Tse <mr.adam.tse@gmail.com>
2016-09-21 23:07:29 -0400

    removed coffee script. causing errors in js load

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $('#pledge-btn').on('click', function(button){
+  $('.pledge-btn').on('click', function(button){
     button.preventDefault();
-    var id = $(this).attr("id");
-    $.ajax({
-      url: 'projects/'+id+'/pledges/new',
-      method: 'GET',
-      data: {},
-      dataType: 'html'
-    }).done(function(responseData){
-      responseData.appendTo($('#pledge-form'));
-    })
-  });
+  var id = $(this).attr("id");
+  $.ajax({
+    url: 'projects/'+id+'/pledges/new',
+    method: 'GET',
+    data: {},
+    dataType: 'html'
+  }).done(function(responseData){
+    responseData.appendTo($('#pledge-form'));
+  })
+});
 
 
 
