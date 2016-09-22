195376947f33b5c05e03d31631146ad7a78234ac
mitchell demers <mitchelldemers66@gmail.com>
2016-09-21 20:18:26 -0400

    added a countdown function

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,42 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.return_due_date(project)
+    project.date.strftime("Campaign ends: %m-%d-%y")
+  end
+
+  def self.get_countdown_time(project)
+    current_time = (Time.new).strftime('%m-%d-%y')
+    due_date = self.get_due_date(project)[15,9]
+
+    year_difference = due_date[6,2].to_i - current_time[6,2].to_i
+    day_difference = due_date[3,2].to_i - current_time[3,2].to_i
+    month_difference = due_date[0,2].to_i - current_time[0,2].to_i
+
+    if (year_difference) <= 0
+      return 'The Project is Expired'
+    end
+
+    if (year_difference ) == 0 && day_difference >= 0
+      months_left = month_difference
+      days_left = day_difference
+      return "the Project has #{months_left} months and #{days_left} days left"
+
+    elsif (year_difference) == 0 && day_difference < 0
+      months_left = (month_difference) - 1
+      days_left = (day_difference) + 30
+      return "the Project has #{months_left} months and #{days_left} days left"
+      
+    elsif (year_difference ) >= 1 && day_difference >= 0
+      months_left = (month_difference) + (12 * year_difference) 
+      days_left = (day_difference)
+      return "the Project has #{months_left} months and #{days_left} days left"
+
+    elsif(year_difference) >= 1 && day_difference < 0
+      months_left = (month_difference) + (12 * year_difference - 1)
+      days_left = (day_difference) + 30
+      return "the Project has #{months_left} months and #{days_left} days left"
+    end
+  end
+
 end
