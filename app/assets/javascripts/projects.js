a65a8b7b5427b29719fe3c538d75d34581dcd0fa
Adam Tse <mr.adam.tse@gmail.com>
2016-09-23 00:08:34 -0400

    passed vara into json for all categories

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -13,16 +13,16 @@ $(function() {
       data: {},
       dataType: 'json'
     }).done(function(data){
-
-      for(i = 0, l = data.length; i < l; i++){
+      console.log(data);
+      for(i = 0, l = data.projects.length; i < l; i++){
         $('<tr>').appendTo('.categories-table')
-        $('<td>').attr('class', 'name').html(data[i]['name']).appendTo('.categories-table')
-        $('<td>').attr('class', 'name').html(data[i]['date']).appendTo('.categories-table')
-        $('<td>').attr('class', 'name').html(data[i]['goal']).appendTo('.categories-table')
-        $('<td>').attr('class', 'name').html(data[i]['user_id']).appendTo('.categories-table')
-        $('<td>').attr('class', 'name').html(data[i]['category_id']).appendTo('.categories-table')
-        $('<td>').attr('class', 'name').html(data[i]['description']).appendTo('.categories-table')
-        $('<a>').attr('class', 'show-btn').attr('href', '/projects/' + (data[i]['id'] +1) ).html('Read More').appendTo('.categories-table')
+        $('<td>').attr('class', 'name').html(data.projects[i]['name']).appendTo('.categories-table')
+        $('<td>').attr('class', 'name').html(data.projects[i]['date']).appendTo('.categories-table')
+        $('<td>').attr('class', 'name').html(data.projects[i]['goal']).appendTo('.categories-table')
+        $('<td>').attr('class', 'name').html(data.users[i]['name']).appendTo('.categories-table')
+        $('<td>').attr('class', 'name').html(data.category['name']).appendTo('.categories-table')
+        $('<td>').attr('class', 'name').html(data.projects[i]['description']).appendTo('.categories-table')
+        $('<a>').attr('class', 'show-btn').attr('href', '/projects/' + (data.projects[i]['id']) ).html('Read More').appendTo('.categories-table')
         $('<br>').appendTo('.categories-table')
       };
     }).fail(function(){
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,11 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @projects = @category.projects
+    @users    = User.all
     respond_to do |format|
       format.html
       #format.json { render json: @rest_hash_arr} # @restaurants}
-      format.json { render json: @projects, local}
+      format.json { render json: {projects: @projects, users: @users,category: @category}}
     end
   end
 end
