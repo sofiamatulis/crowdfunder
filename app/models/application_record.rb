7714676d166fe61307045ae2e2091ac360251951
Adam Tse <mr.adam.tse@gmail.com>
2016-09-23 11:55:32 -0400

    merge

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -55,7 +55,7 @@ class ApplicationRecord < ActiveRecord::Base
     end
     return count
   end
-  
+
   def self.find_rewards(project_id)
     self.where(project_id: project_id)
   end
@@ -74,9 +74,3 @@ class ApplicationRecord < ActiveRecord::Base
 
 
 end
-
-
-
-
-
-
